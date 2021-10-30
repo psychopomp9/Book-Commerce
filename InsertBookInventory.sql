dc1f80bcdcba1f7795cd486cb04af44c36451624
psychopomp9 <bhargavd97@gmail.com>
2021-10-30 15:16:12 -0400

    order table name change, image added, styles and other functionality added

--- InsertBookInventory.sql
+++ InsertBookInventory.sql
@@ -1,8 +1,8 @@
 USE book_commerce;
 
 INSERT INTO item_inventory VALUES 
-	(null, 'SPY X FAMILY', 5), 
-    (null, 'Kaiju No. 8', 10), 
-    (null, 'A Sign of Affection', 6), 
-    (null, 'Blue Lock', 8), 
-    (null, 'Goodnight Punpun', 16);
+	(null, 'SPY X FAMILY', 'spyxfamily.jpg',5), 
+    (null, 'Kaiju No. 8', 'kaiju_no8.jpg',10), 
+    (null, 'A Sign of Affection', 'a_sign_of_affection.jpg',6), 
+    (null, 'Blue Lock', 'blue_lock.jpg',8), 
+    (null, 'Goodnight Punpun', 'goodnight_punpun.jpg',16);
--- check_out.php
+++ check_out.php
@@ -16,22 +16,33 @@
         
         if(mysqli_stmt_num_rows($stmt) != 1){
             $q1 = "INSERT INTO user VALUES (null, '$first_name', '$last_name');";
-            $r1 = @mysqli_query($dbc, $q1);
+            $r1 = mysqli_query($dbc, $q1);
         }
+        
+        mysqli_stmt_close($stmt);
         
         $q2 = "SELECT user_id FROM user WHERE first_name = ? AND last_name = ?;";
         
         $stmt1 = mysqli_prepare($dbc, $q2);
-        mysqli_stmt_bind_param($stmt1, 'ss', $first_name, $last_name);           mysqli_stmt_execute($stmt1);
+        mysqli_stmt_bind_param($stmt1, 'ss', $first_name, $last_name);           
+        mysqli_stmt_execute($stmt1);
             
         mysqli_stmt_bind_result($stmt1, $user_id);
         mysqli_stmt_fetch($stmt1);
             
-        $q3 = "INSERT INTO order VALUES (null, '$payment_method', 1, $user_id, '{$_COOKIE['item']}');"; 
-        $r3 = @mysqli_query($dbc, $q3);
-            
-        echo "Order placed for $first_name $last_name for item: <br>
-                    redirecting to store page";
+        mysqli_stmt_close($stmt1);
+        $q3 = "INSERT INTO orders VALUES (null, '$payment_method', 1, $user_id, {$_COOKIE['item']});";
+        
+        $r3 = mysqli_query($dbc, $q3);
+        
+        $q4 = "UPDATE item_inventory SET stock=stock-1 WHERE item_id={$_COOKIE['item']};";
+        $r4 = mysqli_query($dbc, $q4);
+        
+        echo "<div class='container'>
+                    <h3>Order placed for <b>$first_name $last_name</b></h3>
+                    <h4>Item: <b>{$_COOKIE['item_name']}</b></h4> <br>
+                    <a href='store.php'>go back to store</a>
+              </div>";
             
 //            sleep(5);
 //            header("Location: store.php");
@@ -40,22 +51,41 @@
 
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+        
+        <style>
+            div{
+                text-align: center;
+                padding: 10px;
+            }
+            
+            #nav{
+                text-align: left;
+            }
+        </style>
+        
         <title>Book-Commerce: Check-out</title>
     </head>
     
     <body>
-        <div <?php if($_SERVER['REQUEST_METHOD'] == 'POST'){ echo "style = 'display: none;'";} ?>>
-            <h2>Please provide below information to submit your order: </h2>
-            <form action="check_out.php" method="POST">
-                <p>First Name: <input type="text" name="first_name" required></p>
-                <p>Last Name: <input type="text" name="last_name" required></p>
-<!--                <p>Phone: <input type="text" name="contact" pattern="[0-9]{10}" required></p>-->
-                <p>Payment Options: </p>
-                    <p><input type="radio" name="payment_method" value="credit" checked>Credit Card</p>
-                    <p><input type="radio" name="payment_method" value="debit">Debit Card</p>
-                    <p><input type="radio" name="payment_method" value="interac">Interac</p>
-                <p><input type="submit" value="Pay"></p>
-            </form>
+        <div class="container">
+            <div <?php if($_SERVER['REQUEST_METHOD'] == 'POST'){ echo "style = 'display: none;'";} ?>>
+                <a href="store.php">Back to Store</a><br><br>
+                <h2>Please provide below information to place your order: </h2><br>
+                <form action="check_out.php" method="POST">
+                    <p><b>First Name:</b> <input type="text" name="first_name" required></p>
+                    <p><b>Last Name:</b> <input type="text" name="last_name" required></p>
+    <!--                <p>Phone: <input type="text" name="contact" pattern="[0-9]{10}" required></p>-->
+                    <p><b>Payment Options:</b> </p>
+                        <p><input type="radio" name="payment_method" value="credit" checked> Credit Card</p>
+                        <p><input type="radio" name="payment_method" value="debit"> Debit Card</p>
+                        <p><input type="radio" name="payment_method" value="interac"> Interac</p>
+                    <p><input type="submit" value="Pay"></p>
+                </form>
+            </div>
         </div>
     </body>
 </html>
